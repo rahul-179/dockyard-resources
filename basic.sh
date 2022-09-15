a489ecd37e387d04abfb7f71f7ecfeb099827c06
rahul-179 <rahul.malhotra@oneconvergence.com>
2022-09-16 00:52:04 +0530

    Update basic.sh and Add stress.sh

--- basic.sh
+++ basic.sh
@@ -1,28 +1,30 @@
 #!/bin/bash -x
 
+e=0;
 #install package
-sudo apt-get -qq update -y || { echo "command: apt update FAILED"; }
-sudo apt-get -qq install curl -y || { echo"command: apt install curl FAILED"; }
+sudo apt-get -qq update -y || { echo "command: apt update FAILED"; e=1; }
+sudo apt-get -qq install curl -y || { echo"command: apt install curl FAILED"; e=1; }
 
 # curl access
-curl https://github.com/oneconvergence/dkube-examples/blob/tensorflow/README.md || { echo "command: curl https://github.com/oneconvergence/dkube-examples/blob/tensorflow/README.md FAILED"; }
+curl https://github.com/oneconvergence/dkube-examples/blob/tensorflow/README.md || { echo "command: curl https://github.com/oneconvergence/dkube-examples/blob/tensorflow/README.md FAILED"; e=1; }
 
 #Cloning git repository
 [[ -d "./dockyard-resources" ]]  &&  sudo rm -rf ./dockyard-resources
-git clone https://github.com/oneconvergence/dkube-examples.git || { echo "command: git clone https://github.com/oneconvergence/dkube-examples.git FAILED"; }
+git clone https://github.com/oneconvergence/dkube-examples.git || { echo "command: git clone https://github.com/oneconvergence/dkube-examples.git FAILED"; e=1; }
 
 #aws s3 access 
-aws sts get-caller-identity || { echo "command: aws sts get-caller-identity FAILED"; }
+aws sts get-caller-identity || { echo "command: aws sts get-caller-identity FAILED"; e=1; }
 
-aws s3 cp s3://altos-oneconvergence-tfstate/root.tfstate . || { echo "command: aws s3 cp to current dir FAILED"; }
+aws s3 cp s3://altos-oneconvergence-tfstate/root.tfstate . || { echo "command: aws s3 cp to current dir FAILED"; e=1; }
 
-aws s3 cp s3://altos-oneconvergence-tfstate/root.tfstate /home/default && { echo "command: aws s3 cp s3://altos-oneconvergence-tfstate/root.tfstate /home/default PASSED UNEXPECTED" && sudo rm -rf /home/default/root.tf; }
+aws s3 cp s3://altos-oneconvergence-tfstate/root.tfstate /home/default && { echo "command: aws s3 cp s3://altos-oneconvergence-tfstate/root.tfstate /home/default PASSED UNEXPECTED" && sudo rm -rf /home/default/root.tf; e=1; }
 
 #Verify read/write access on other user
 
-touch /home/default/testfile && { echo "command: touch /home/default/testfile PASSED UNEXPECTED"; rm /home/default/testfile; }
-touch test || { echo "command: touch FAILED"; }
+touch /home/default/testfile && { echo "command: touch /home/default/testfile PASSED UNEXPECTED"; rm /home/default/testfile; e=1; }
+touch test || { echo "command: touch FAILED"; e=1; }
 
 #Changing file permissions
-sudo chmod 777 /home/default && { echo "command: sudo chmod 777 /home/default PASSED UNEXPECTED"; sudo chmod 755 /home/default; }
-sudo chown bsabata:bsabata /home/default && { echo "command: sudo chown bsabata:bsabata /home/default PASSED UNEXPECTED"; sudo chown default:default /home/default; }
+sudo chmod 777 /home/default && { echo "command: sudo chmod 777 /home/default PASSED UNEXPECTED"; sudo chmod 755 /home/default; e=1; }
+sudo chown bsabata:bsabata /home/default && { echo "command: sudo chown bsabata:bsabata /home/default PASSED UNEXPECTED"; sudo chown default:default /home/default; e=1; }
+[[ ${e} == 0 ]] && echo "SCRIPT RAN SUCESSFULLY";
---
+++ stress.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+
+#packages
+#install_eksctl() {
+#curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+#sudo mv /tmp/eksctl /usr/local/bin
+#}
+
+e=0
+
+#Get Information
+cpu_core=$(getconf _NPROCESSORS_ONLN)
+free_memory=$(free -m | awk 'NR==2 { print $7 }')
+available_space=$(df -h | awk '{ print $4, $6 }' | grep -w "/" | awk '{ print $1 }' | tr -d G)
+
+cpu=$(( ${cpu_core}/2))
+space=$(( ${available_space}*70/100 ))
+mem=$(( ${free_memory}/256*80/100 ))
+
+echo ${cpu}
+echo ${mem}
+echo ${space}
+
+#command -v eksctl && [[  $? -eq 0  ]] || install_eksctl
+#eksctl get nodegroup --cluster $1 --region us-west-2
+command -v stress-ng && [[  $? -eq 0  ]] || sudo apt-get -qq install stress-ng -y
+
+stress-ng -m ${mem} -c ${cpu} -l 75 % -d ${space} -t $1 || { echo "command: stress-ng FAILED"; e=1; }
+
+#eksctl get nodegroup --cluster $1 --region us-west-2
+[[ ${e} == 0 ]] && echo "SCRIPT RAN SUCESSFULLY";
